c8c92f6c552e1220df1e77e7282cb8466fc2f2fe
Arjit Agarwal <56030842+Cynamide@users.noreply.github.com>
2025-06-02 13:54:05 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,20 @@
-import preswald
-print("Start your project here!")
+from preswald import connect, get_df
+
+connect()  # Initialize connection to preswald.toml data sources
+df = get_df("sample_csv")  # Load data
+
+from preswald import query
+
+sql = "SELECT * FROM sample_csv"
+filtered_df = query(sql, "sample_csv")
+
+from preswald import table, text
+
+text("# My Data Analysis App")
+table(filtered_df, title="Filtered Data")
+
+from preswald import plotly
+import plotly.express as px
+
+fig = px.scatter(df, x="job_title", y="salary_usd", color="category")
+plotly(fig)
